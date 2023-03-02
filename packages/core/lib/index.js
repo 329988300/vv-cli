640711252cd738cd17f711706c860eb349cf72e0
Allen <329988300@qq.com>
2023-03-03 00:11:48 +0800

    yargs init

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,7 +1,15 @@
-'use strict';
+#!/usr/bin/env node
 
-module.exports = core;
+const yargs = require("yargs");
+const {hideBin} = require("yargs/helpers");
+const arg = hideBin(process.argv)
+const cli = yargs(arg)
 
-function core() {
-  return 'Hello from core';
-}
+
+
+cli.usage("Usage: $0 <command> [options]")
+    .alias("h", "help")
+    .alias("v", "version")
+    .wrap(cli.terminalWidth())
+    .epilogue("hello vv-cli")
+    .argv;
